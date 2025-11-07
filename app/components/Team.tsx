c2885cac19a45461d752453ff5a93d4b91d56d67
morethanyouth1nk <tolgaakyol45@hotmail.com>
2025-11-07 16:16:49 +0300

    Adjusted team grid to 2 columns for better alignment

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -53,7 +53,7 @@ const team = [
         </motion.div>
 
         {/* Team Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {team.map((member, index) => (
             <motion.div
               key={index}
