aecd261195a519bced86dbe643e1244560144217
morethanyouth1nk <tolgaakyol45@hotmail.com>
2026-05-16 19:47:05 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import FutureWork from './components/FutureWork';
 import Team from './components/Team';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -15,6 +16,7 @@ export default function Home() {
       <About />
       <Features />
       <HowItWorks />
+      <FutureWork />
       <Team />
       <Contact />
       <Footer />
